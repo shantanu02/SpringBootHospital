66ca64412baef939ddce333ea2995e10b88426fd
shantanu02 <shantanuupase02@gmail.com>
2021-01-22 01:53:11 -0600

    more native queries added

--- src/main/java/com/MGM/HospitalManagement/Service/PatientServiceImple.java
+++ src/main/java/com/MGM/HospitalManagement/Service/PatientServiceImple.java
@@ -265,4 +265,11 @@ public class PatientServiceImple implements PatientService {
 
 	}
 
+	@Override
+	public List<Integer> getPtIdbyPatientId(int patientId) {
+		return patientTreatmentRepo.getPatientTreatmentIdByPatientId(patientId);
+	}
+
+
+
 }
